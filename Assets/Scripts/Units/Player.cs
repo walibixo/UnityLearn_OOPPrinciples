cdb6e978d1536015d668b2053fa238786eea35d6
walibixo <chatot.antoine@gmail.com>
2024-09-08 22:37:14 +0200

    Add Sqash and Stretch animation on shoot

--- Assets/Scripts/Units/Player.cs
+++ Assets/Scripts/Units/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : Unit
@@ -5,35 +6,39 @@ public class Player : Unit
     public GameObject projectilePrefab;
 
     private Camera mainCamera;
+    private new Rigidbody rigidbody;
+
+    private Vector3 originalScale;
+    private Vector3 moveInput;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, -1, 0);
-        speed = 5.0f;
+        speed = 4.0f;
         range = 10.0f;
 
         mainCamera = Camera.main;
+        rigidbody = GetComponent<Rigidbody>();
+
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveInput.z = Input.GetAxisRaw("Vertical") * speed;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var hInput = Input.GetAxis("Horizontal");
-        var vInput = Input.GetAxis("Vertical");
-
-        var moveDirection = new Vector3(hInput, 0, vInput);
-        moveDirection.Normalize();
-
-        transform.Translate(speed * Time.deltaTime * moveDirection);
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Attack();
-        }
+        rigidbody.velocity = moveInput;
     }
 
     public override void Attack()
@@ -64,6 +69,33 @@ public class Player : Unit
         projectile.transform.Rotate(Vector3.up, Random.Range(-50.0f, 50.0f));
 
         Destroy(projectile, 2.0f);
+
+        StartCoroutine(SqashAndStretch());
+    }
+
+    IEnumerator SqashAndStretch()
+    {
+        var squashScale = originalScale * 0.8f;
+
+        // Use Lerp to smoothly transition between the original scale and the squash scale
+        for (float t = 0; t < 0.05f; t += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, squashScale, t / 0.05f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        var stretchScale = originalScale * 1.2f;
+        for (float t = 0; t < 0.1f; t += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(squashScale, stretchScale, t / 0.1f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        for (float t = 0; t < 0.05f; t += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(stretchScale, originalScale, t / 0.05f);
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     private (bool success, Vector3 position) GetMousePosition()
